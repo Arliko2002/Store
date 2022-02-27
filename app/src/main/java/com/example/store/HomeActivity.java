de65bd059298218464b6e9dbac08a5bbac1ce87b
Arliko2002 <77797439+Arliko2002@users.noreply.github.com>
2022-02-28 00:50:12 +0600

    Drawer

--- app/src/main/java/com/example/store/HomeActivity.java
+++ app/src/main/java/com/example/store/HomeActivity.java
@@ -1,16 +1,17 @@
 package com.example.store;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-
 import com.example.store.fragments.FCategries;
 import com.example.store.fragments.FShoppingCart;
 import com.example.store.fragments.HomeFragment;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity{
     ChipNavigationBar chipNavigationBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +20,19 @@ public class HomeActivity extends AppCompatActivity {
 
         chipNavigationBar = findViewById(R.id.bottom_nav);
         bottomMenu();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        toolbar.setTitle("Sliding Root");
+        setSupportActionBar(toolbar);
+
+        new SlidingRootNavBuilder(this)
+                .withToolbarMenuToggle(toolbar)
+                .withMenuOpened(false)
+                .withMenuLayout(R.layout.menu_left_drawer)
+                .inject();
     }
+
+
 
     private void bottomMenu() {
         chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
