2d1be191f0169e93dad746c4ebbf29e28af2427d
Arliko2002 <77797439+Arliko2002@users.noreply.github.com>
2022-02-28 15:00:59 +0600

    Basket

--- app/src/main/java/com/example/store/fragments/FCategries.java
+++ app/src/main/java/com/example/store/fragments/FCategries.java
@@ -10,11 +10,7 @@ import android.view.ViewGroup;
 
 import com.example.store.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FCategries#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FCategries extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
--- app/src/main/java/com/example/store/fragments/HomeFragment.java
+++ app/src/main/java/com/example/store/fragments/HomeFragment.java
@@ -2,8 +2,12 @@ package com.example.store.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,50 +15,16 @@ import android.view.ViewGroup;
 
 import com.example.store.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_f_categries, container, false);
     }
 }
